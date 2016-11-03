f755481f3980e39f90ce001995728f5cbbfc7c89
Ruotao He <ruotaohe@163.com>
2016-11-03 23:09:54 +0800

    Add init calibration client and capture pattern

--- src/extrinsic_calibration.cpp
+++ src/extrinsic_calibration.cpp
@@ -6,6 +6,8 @@
 #include <tf/transform_listener.h>
 #include <tf_conversions/tf_eigen.h>
 #include <eigen_conversions/eigen_msg.h>
+#include <ensenso/CapturePattern.h>
+#include <ensenso/InitCalibration.h>
 
 #include <string>
 
@@ -59,8 +61,15 @@ Eigen::Affine3d generateRandomHemispherePose(const Eigen::Vector3d &obj_origin, 
 class ex_cal
 {
     ros::NodeHandle nh;
+    //Publisher
     ros::Publisher status_pub;
     ros::Publisher marker_pub;
+    //Service client
+    ros::ServiceClient capture_pattern_client;
+    ros::ServiceClient init_cal_client;
+    //Service srv
+    ensenso::InitCalibration init_cal_srv;
+    ensenso::CapturePattern capture_pattern_srv;
 
 public:
     ex_cal(int num_poses,std::string tcp_name):
@@ -70,6 +79,9 @@ public:
         //initialize publisher
         status_pub=nh.advertise<std_msgs::String>("ensenso_calibration_status",1);
         marker_pub=nh.advertise<visualization_msgs::Marker>("hemisphere",1);
+        //initialize service client
+        capture_pattern_client = nh.serviceClient<ensenso::CapturePattern>("capture_pattern");
+        init_cal_client = nh.serviceClient<ensenso::InitCalibration>("init_calibration");
     }
 
     bool performCalibration()
@@ -78,8 +90,8 @@ public:
         tf::TransformListener listener;
         listener.waitForTransform("/base",tcp_name_,ros::Time::now(),ros::Duration(3.0));
         tf::StampedTransform transform_stamped;
-        Eigen::Affine3d initial_pose;
 
+        Eigen::Affine3d initial_pose;
         std_msgs::String status;
         try{
             listener.lookupTransform("/base",tcp_name_,ros::Time(0),transform_stamped);
@@ -92,6 +104,7 @@ public:
             return false;
         }
 
+        //Generate hemisphere for robot poses generation
         Eigen::Vector3d tool_origin(initial_pose.translation()[0],
                                     initial_pose.translation()[1],
                                     initial_pose.translation()[2]),obj_origin(tool_origin[0],tool_origin[1],tool_origin[2]-calTabDistance/1000.0);
@@ -110,19 +123,31 @@ public:
         std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d> > robot_poses;
         int fail_count=0;
 
+        //Initialize Calibration (Set grid space & Clear buffer)
+        ros::service::waitForService("init_calibration");
+        init_cal_srv.request.grid_spacing=20.0;
+        init_cal_client.call(init_cal_srv);
+        if(!init_cal_srv.response.success){
+            ROS_ERROR("Initialize calibration fail!");
+            return false;
+        }
+
         while(nh.ok() && robot_poses.size()<num_poses_)
         {
-            if(fail_count>3){
+            geometry_msgs::Pose wayPoint;
+
+            if(fail_count>5){
                 //Deal with failure...
                 ROS_ERROR("Too many failures! Abort calibration!");
 
                 //Go back to initial pose
+                tf::poseEigenToMsg(initial_pose,wayPoint);
                     //...
 
                 return false;
             }
 
-            geometry_msgs::Pose wayPoint;
+
             tf::poseEigenToMsg(generateRandomHemispherePose(obj_origin,tool_origin),wayPoint);
             //Move UR to wayPoint
                 //...
@@ -130,7 +155,17 @@ public:
             //sleep until UR reach the goal point
             sleep(1);
 
-            //collect pattern
+            //Collect patterns
+            //block until service available
+            ros::service::waitForService("capture_pattern");
+            capture_pattern_client.call(capture_pattern_srv);
+
+            if(!capture_pattern_srv.response.success)
+            {
+                fail_count++;
+                //Move UR to defalut pose
+                continue;
+            }
 
             //collect robot pose
             try{
@@ -173,6 +208,33 @@ public:
 
 int main(int argc, char** argv)
 {
+
+    ros::init(argc,argv,"extrin_cal");
+    ros::NodeHandle nh;
+
+    //Service client
+    ros::ServiceClient capture_pattern_client=nh.serviceClient<ensenso::CapturePattern>("capture_pattern");
+    ros::ServiceClient ini_cal_client=nh.serviceClient<ensenso::InitCalibration>("init_calibration");
+    //Service srv
+    ensenso::InitCalibration ini_cal_srv;
+    ini_cal_srv.request.grid_spacing=20.0;
+    ros::service::waitForService("init_calibration");
+    ini_cal_client.call(ini_cal_srv);
+    bool is_success=ini_cal_srv.response.success;
+
+    int num_pattern=5;
+    int i=0;
+    ensenso::CapturePattern capture_pattern_srv;
+    while(i<num_pattern)
+    {
+        ros::service::waitForService("capture_pattern");
+        capture_pattern_client.call(capture_pattern_srv);
+        bool success = capture_pattern_srv.response.success;
+        i=capture_pattern_srv.response.pattern_count;
+        sleep(1);
+    }
+
+
 
     return 0;
 }
